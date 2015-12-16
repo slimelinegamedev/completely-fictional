04730d0145bb16c8b542a85a57497261a8c713ed
ledge23 <zs.schuster@gmail.com>
2015-12-15 21:30:11 -0800

    repeater-ized the footer :D

--- src/footer-index.js
+++ src/footer-index.js
@@ -2,6 +2,11 @@ import {bindable} from 'aurelia-framework';
 
 export class FooterIndex {
 	currentYear = new Date().getFullYear();
-	
+	footerLinks = [
+		{ url: 'http://medium.com/@destroyvegas', img : `medium.jpeg`, alt: 'Medium' },
+		{ url: 'http://www.twitter.com/destroyvegas', img : `twitter.png`, alt: 'Twitter' },
+		{ url: 'http://somewhatnonfictional.tumblr.com', img : `tumblr.png`, alt: 'Tumblr' },
+		{ url: 'http://www.flickr.com/photos/zackschuster/', img : `flickr.png`, alt: 'Flickr' },
+	];
   @bindable router = null;
 }
