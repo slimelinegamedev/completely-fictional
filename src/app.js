36cd3059102e6c1a0e633acfcedbcccd1362c71c
ledge23 <zs.schuster@gmail.com>
2015-12-15 18:22:12 -0800

    added base nav routes

--- src/app.js
+++ src/app.js
@@ -3,14 +3,14 @@ export class App {
     config.title = 'Completely Fictional';
     config.map([
       { route: [''], name: 'home', moduleId: 'home', nav: false },
+      { route: ['poetry'], name: 'poetry', moduleId: 'poetry', nav: true, title: 'Poetry' },
+      { route: ['essays'], name: 'essays', moduleId: 'essays', nav: true, title: 'Essays' },
+      { route: ['reviews'], name: 'reviews', moduleId: 'reviews', nav: true, title: 'Reviews' },
+      { route: ['trackback'], name: 'trackback', moduleId: 'trackback', nav: true, title: 'Trackback' },
+      { route: ['large-round-eyes'], name: 'large-round-eyes', moduleId: 'large-round-eyes', nav: true, title: 'Large Round Eyes' },
+      { route: ['extras'], name: 'extras', moduleId: 'extras', nav: true, title: 'Extras' },
     ]);
-
-        // Poetry
-        // Essays
-        // Reviews
-        // Trackback
-        // Large Round Eyes
-        // Extras
+    
     this.router = router;
   }
 }
