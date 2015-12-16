e83dca831a9f28d4945647a9bdc56ee48bf78c51
ledge23 <zs.schuster@gmail.com>
2015-12-15 21:57:09 -0800

    cleaned up parent router syntax

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Completely Fictional';
     config.map([
-      { route: [''], name: 'home', moduleId: 'home', nav: false },
-      { route: ['poetry'], name: 'poetry', moduleId: 'poetry', nav: true, title: 'Poetry' },
-      { route: ['essays'], name: 'essays', moduleId: 'essays', nav: true, title: 'Essays' },
-      { route: ['reviews'], name: 'reviews', moduleId: 'reviews', nav: true, title: 'Reviews' },
-      { route: ['trackback'], name: 'trackback', moduleId: 'trackback', nav: true, title: 'Trackback' },
-      { route: ['large-round-eyes'], name: 'large-round-eyes', moduleId: 'large-round-eyes', nav: true, title: 'Large Round Eyes' },
-      { route: ['extras'], name: 'extras', moduleId: 'extras', nav: true, title: 'Extras' },
+      { route: '', name: 'home', moduleId: 'home', nav: false },
+      { route: 'poetry', name: 'poetry', moduleId: 'poetry', nav: true, title: 'Poetry' },
+      { route: 'essays', name: 'essays', moduleId: 'essays', nav: true, title: 'Essays' },
+      { route: 'reviews', name: 'reviews', moduleId: 'reviews', nav: true, title: 'Reviews' },
+      { route: 'trackback', name: 'trackback', moduleId: 'trackback', nav: true, title: 'Trackback' },
+      { route: 'large-round-eyes', name: 'large-round-eyes', moduleId: 'large-round-eyes', nav: true, title: 'Large Round Eyes' },
+      { route: 'extras', name: 'extras', moduleId: 'extras', nav: true, title: 'Extras' },
     ]);
     
     this.router = router;
