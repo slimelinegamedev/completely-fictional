f673545cdc2cd0a1775c49dc63e0ec2d93c3c786
ledge23 <zs.schuster@gmail.com>
2016-01-24 23:18:32 -0800

    Shorten build to just compile JS
    (It's all I really need now)

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,10 @@
 var gulp = require('gulp');
-var jade = require('gulp-jade');
 var concat = require('gulp-concat');
 var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
 
-var root = require('./build/paths').app;
-var dest = require('./build/paths').output;
-var tree = require('./build/paths').tree;
-var glob = require('./build/paths').glob;
-
-
 gulp.task('default', function () {
-  gulp.watch(tree.jade + glob.jade, { cwd: root }, ['html']);
-  gulp.watch(tree.js + glob.js, { cwd: root }, ['js']);
-});
-
-gulp.task('build', ['html', 'js']);
-
-gulp.task('html', function () {
-  gulp.src(tree.jade + glob.jade, { cwd: root })
-    .pipe(jade())
-    .pipe(gulp.dest(tree.html, { cwd: dest }));
-});
-
-gulp.task('js', function () {
-  gulp.src(glob.js, { cwd: tree.js })
+  gulp.src('**/*.js', { cwd: 'js' })
     .pipe(concat('app.js'))
     .pipe(babel())
     .pipe(uglify())
