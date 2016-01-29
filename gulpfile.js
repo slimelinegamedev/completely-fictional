62672169625ae92f6f67870c4afc5898ff25e8bd
ledge23 <zs.schuster@gmail.com>
2016-01-29 01:12:11 -0800

    Transition to Autobundler

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
-var babel = require('gulp-babel');
-var uglify = require('gulp-uglify');
+var autobundler = require('autobundler');
 
 gulp.task('default', function () {
-  gulp.src('**/*.js', { cwd: 'js' })
-    .pipe(concat('app.js'))
-    .pipe(babel())
-    .pipe(uglify())
-    .pipe(gulp.dest('scripts'));
+  autobundler({
+    source: 'js',
+    moduleDir: 'modules',
+    destination: 'scripts',
+  });
 });
 
 gulp.start('default');
